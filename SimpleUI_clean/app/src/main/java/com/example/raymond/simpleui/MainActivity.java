921db6f5b3f06288906f4d7063b7db12e9737d5f
rmf770609 <tienhungfong@gmail.com>
2016-03-08 09:59:50 +0800

    [JAVA] Finish keyboard detected, read edit text, set text view, hide checkbox

--- SimpleUI_clean/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
+++ SimpleUI_clean/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
@@ -2,12 +2,71 @@ package com.example.raymond.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Declare variables */
+    TextView textView;
+    EditText editText;
+    CheckBox hidecheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Path:res/layout/activity_main.xml
+
+        /* Put value into variables */
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        hidecheckBox = (CheckBox)findViewById(R.id.hide_checkBox);
+
+        /* Keyboard detected */
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        /* Virtual keyboard detected */
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    /* onClick event @button */
+    public void submit(View view)
+    {
+        String text = editText.getText().toString(); //get string from edit text
+        if (hidecheckBox.isChecked())
+        {
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            textView.setText("**********");
+            editText.setText("**********");
+        }
+        else
+        {
+            textView.setText(text);
+        }
+        editText.setText("");
     }
 }
