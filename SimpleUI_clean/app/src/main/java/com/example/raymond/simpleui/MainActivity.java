bb35317c06545fce7531a9eabfac8b1e4f6feeea
rmf770609 <tienhungfong@gmail.com>
2016-03-14 10:05:25 +0800

    Add btnClean to clean history.txt
    * Ask why need MainActivity.this
    * editor.clear only clean SharedPreferences

--- SimpleUI_clean/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
+++ SimpleUI_clean/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     CheckBox hidecheckBox;
     Button button_restore;
+    Button btnClean;
     SharedPreferences sp;
     SharedPreferences.Editor editor;
+    ListView listView;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +39,16 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         hidecheckBox = (CheckBox)findViewById(R.id.hide_checkBox);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         /* Setup button_restore & related triggered event */
         button_restore = (Button)findViewById(R.id.button_restore);
         button_restore.setOnClickListener(button_restoreOnClick);
+        /* Setup btnClean & related clean SP event */
+        btnClean = (Button)findViewById(R.id.btnClean);
+        btnClean.setOnClickListener(btnCleanOnClick);
+
         /* Setup Shared Preference */
         sp = getSharedPreferences("setting", Context.MODE_PRIVATE);
         editor = sp.edit();
@@ -74,12 +86,32 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        setListView();
+        setSpinner();
+    }
+
+    /* Set listView */
+    private void setListView()
+    {
+        String[] data = Utils.readFile(this, "history.txt").split("\n"); //.split() NEED to check further
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        listView.setAdapter(adapter);
+    }
+
+    /* Set Spinner */
+    private void setSpinner()
+    {
+        String[] data = getResources().getStringArray(R.array.storeInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        spinner.setAdapter(adapter);
     }
 
     /* onClick event @button */
     public void submit(View view)
     {
         String text = editText.getText().toString(); //get string from edit text
+        Utils.writeFile(this, "history.txt", text + '\n');
         if (hidecheckBox.isChecked())
         {
             Toast.makeText(this, text, Toast.LENGTH_LONG).show();
@@ -90,6 +122,8 @@ public class MainActivity extends AppCompatActivity {
         {
             textView.setText(text);
         }
+        /* Show in listview */
+        setListView();
         /* Put into SP */
         editor.putString("editText" , editText.getText().toString());
         editor.apply();
@@ -104,6 +138,17 @@ public class MainActivity extends AppCompatActivity {
         {
             editText.setText(sp.getString("editText" , ""));
             //editText.setText("RESTORE");
+        }
+    };
+
+    /* onCLick event @btnClean */
+    private Button.OnClickListener btnCleanOnClick = new Button.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            Utils.cleanFile(MainActivity.this, "history.txt", "");
+            Toast.makeText(MainActivity.this, "Clean", Toast.LENGTH_LONG).show(); //WHY cannot this
+            setListView();
         }
     };
 }
